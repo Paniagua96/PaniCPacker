d4ea5c5d4bc81642114d8ee316f59202766b051c
Paniagua96 <paniaguad9@gmail.com>
2026-07-28 14:25:19 -0600

    WIP UI

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QVBoxLayout>
+#include <QLabel>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    //Initialize loop (update)
     return QApplication::exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QLabel>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
+    //Window title
+    setWindowTitle("PaniCPacker");
+
+    //Set initial size
+    resize(1000,650);
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::exportTexture()
+{
+
 }
