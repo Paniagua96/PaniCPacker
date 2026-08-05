e35d45557b7f546cf64586b7bc2766a700dc87c7
Paniagua96 <paniaguad9@gmail.com>
2026-08-05 12:02:47 -0600

    Add - Qt License

--- popup_about.cpp
+++ popup_about.cpp
@@ -9,6 +9,17 @@ PopupAbout::PopupAbout(QWidget *parent) : QDialog(parent), ui(new Ui::Popup_Abou
 
     //Build using main window.ui (ui from designer)
     ui->setupUi(this);
+
+    //Signal btn about Qt
+    connect(
+        ui->btn_aboutQt,
+        &QPushButton::clicked,
+        this,
+        [this]()
+        {
+            QApplication::aboutQt();
+        }
+        );
 }
 
 PopupAbout::~PopupAbout()
